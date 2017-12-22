970150ec627b8ab2b26d9378d52056ed5b0451fb
ASHISH2796 <ashishgupta2796@gmail.com>
2017-12-22 17:15:12 +0530

    Added logging with logback

--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -1,22 +1,29 @@
 package com.devopsbuddy;
 
+import com.devopsbuddy.web.i18n.I18NService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes =DevopsbuddyApplication.class)
+//@SpringApplicationConfiguration(classes =DevopsbuddyApplication.class)
 //@RunWith(SpringRunner.class)
-//@SpringBootTest
+@SpringBootTest
 @WebAppConfiguration
 public class DevopsbuddyApplicationTests {
 
+	@Autowired
+	private I18NService i18NService;
+
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocaleService() throws Exception {
+		String expectedResult ="Bootstrap starter template";
+		String messageId ="index.main.callouts";
+		String actual = i18NService.getMessage(messageId);
+
 	}
 
 }
